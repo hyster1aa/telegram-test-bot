159252c66b538cc603c10f7054335a98ecda6fa6
hyster1aa <mixail.demen.2002@gmail.com>
2023-08-04 16:19:32 +0500

    Bot-Reply

--- main/main/Program.cs
+++ main/main/Program.cs
@@ -1,2 +1,53 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Telegram.Bot;
+using Telegram.Bot.Types;
+using Telegram.Bot.Exceptions;
+using Telegram.Bot.Polling;
+using Telegram.Bot.Types.ReplyMarkups;
+
+
+namespace telegrambot
+{
+    class Program
+    {
+        private static string token = "5771619188:AAFazCfqGR_VbvCx6ODSAER0DGXKJz15cDc";
+        static ITelegramBotClient bot = new TelegramBotClient(token);
+        public static async Task HandleUpdateAsync(ITelegramBotClient botClient, Update update, CancellationToken cancellationToken)
+        {
+            var message = update.Message;
+            Console.WriteLine($"{message.Chat.FirstName}    |   {message.Text}");
+            if (message != null)
+            {   
+                if (message.Text.ToLower().Contains("привет"))
+                {
+                    await botClient.SendTextMessageAsync(message.Chat, $"Bye");
+                }
+
+                await botClient.SendTextMessageAsync(message.Chat, $"You said:\n{message.Text}");
+            }
+        }
+        public static async Task HandleErrorAsync(ITelegramBotClient botClient, Exception exception, CancellationToken cancellationToken)
+        {
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Запущен бот " + bot.GetMeAsync().Result.FirstName);
+
+            var cts = new CancellationTokenSource();
+            var cancellationToken = cts.Token;
+            var receiverOptions = new ReceiverOptions
+            {
+                AllowedUpdates = { }, // receive all update types
+            };
+            bot.StartReceiving(
+                HandleUpdateAsync,
+                HandleErrorAsync,
+                receiverOptions,
+                cancellationToken
+            );
+            Console.ReadLine();
+        }
+    }
+}
