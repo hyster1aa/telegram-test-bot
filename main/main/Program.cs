c0b5d9cc687572083a636d4505edc9e2ff7fbb8e
hyster1aa <mixail.demen.2002@gmail.com>
2024-02-09 15:30:06 +0500

    last version. unfinished

--- main/main/Program.cs
+++ main/main/Program.cs
@@ -11,24 +11,27 @@ using System.Net;
 using HHmodel;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
-
+using System.Reflection.Metadata.Ecma335;
+//попытка написать бота была на 3ем курсе
 namespace telegrambot
 {
     //https://dev.to/osempu/build-a-web-api-with-c-asp-net-core-70-jp
     class Program
     {
+        static VacanciesList somelist = new VacanciesList();
         private static string token = "5771619188:AAFazCfqGR_VbvCx6ODSAER0DGXKJz15cDc";
         static ITelegramBotClient bot = new TelegramBotClient(token);
-
-        public static void GetVacansy(string url)
+        public static int counter = 1;
+        public static VacanciesList GetVacansy(string url)
         {
+            VacanciesList VacancyLst = new VacanciesList();
             using (var client = new HttpClient())
             {
                 client.DefaultRequestHeaders.UserAgent.ParseAdd("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36 OPR/94.0.0.0");
                 var endpoint = new Uri(url);
                 var result = client.GetAsync(endpoint).Result;
                 var jsn = result.Content.ReadAsStringAsync().Result;
-                VacanciesList VacancyLst = new VacanciesList();
+               
                 try
                 {
                     VacancyLst = JsonConvert.DeserializeObject<VacanciesList>(jsn);
@@ -38,25 +41,34 @@ namespace telegrambot
                 {
                     Console.WriteLine(ex.ToString());
                 }
-                foreach (var item in VacancyLst.list)
-                {
-                    Console.WriteLine(item.name+" "+item.url+" "+item.area.name);
-                }
             }
+            return VacancyLst;
         }
         public static async Task HandleUpdateAsync(ITelegramBotClient botClient, Update update, CancellationToken cancellationToken)
         {
             var message = update.Message;
             Console.WriteLine($"{message.Chat.FirstName}    |   {message.Text}");
             if (message != null)
-            {   
-                if (message.Text.ToLower().Contains("привет"))
-                {
-                    await botClient.SendTextMessageAsync(message.Chat, $"Bye");
-                    
-                }
-                GetVacansy($"https://api.hh.ru/vacancies?text={message.Text}");
+            {
+                VacanciesList somelist = GetVacansy($"https://api.hh.ru/vacancies?text={message.Text}");
                 await botClient.SendTextMessageAsync(message.Chat, $"You said:\n{message.Text}");
+                try
+                {
+                    if (counter==0)
+                    {
+                        await botClient.SendTextMessageAsync(message.Chat, somelist.list[counter].name + " " + somelist.list[counter].url + " " + somelist.list[counter].area.name);
+                    }
+                    if(counter!=0 && message.Text.ToLower().Contains("дальше") || message.Text.ToLower().Contains("next"))
+                    {
+                        await botClient.SendTextMessageAsync(message.Chat, somelist.list[counter].name + " " + somelist.list[counter].url + " " + somelist.list[counter].area.name);
+                    }
+                    
+                    counter++;
+                }
+                catch
+                {
+                    await Console.Out.WriteLineAsync("Неудача");
+                }
             }
         }
         public static async Task HandleErrorAsync(ITelegramBotClient botClient, Exception exception, CancellationToken cancellationToken)
